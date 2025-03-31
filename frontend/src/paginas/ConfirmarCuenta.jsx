bc9774b69a59c091c26ae9ff5410274bc0752044
Rodrigo <rodrigomastache@gmail.com>
2025-03-31 15:47:01 -0600

    feat: se desarrolla la confirmacion de la cuenta y se agregan variables de entorno

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,7 +1,68 @@
+// Importar useEffect para ejecute un codigo una vez que el componente este listo
+import { useEffect, useState } from "react";
+// Importando hook "useParams" para extraer datos de la url
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alerta from "./../components/Alerta";
 const ConfirmarCuenta = () => {
+  // Por default la cuenta no va a estar confirmada "false"
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+  // Estado de espera en lo que arroja el resultado
+  const [cargando, setCargando] = useState(true);
+  // Estado para la alerta
+  const [alerta, setAlerta] = useState({});
+  // Leer el token de la url
+  const params = useParams();
+  const { id } = params;
+
+  // Una vez el componente este listo, ejecutar:
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+        // Se usa la variable de entorno del backend
+        const url = `${
+          import.meta.env.VITE_BACKEND_URL
+        }/api/veterinarios/confirmar/${id}`;
+
+        // Respuesta de axios. (por default es "get", por lo que se puede omitir)
+        const { data } = await axios(url);
+        // Se confirmo la cuenta
+        setCuentaConfirmada(true);
+        // Establecer alerta de cuenta confirmada
+        setAlerta({
+          msg: data.msg,
+        });
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+      // ya sea que haya un error o se haya cambiado correctamente, cambia a false
+      setCargando(false);
+    };
+    confirmarCuenta();
+  }, []);
+
   return (
     <>
-      <h1>Desde "Confirmar Cuenta"</h1>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Confirma tu cuenta y comienza a administrar {""}
+          <span className="text-black">tus pacientes</span>
+        </h1>
+      </div>
+
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-x bg-white">
+        {/* Cuando ya no este cargando, es decir, ya no este en true, entonces muestra la alerta */}
+        {!cargando && <Alerta alerta={alerta} />}
+        {/* Condicion: si esta confirmada la cuenta  mostrar ligas de iniciar sesión */}
+        {cuentaConfirmada && (
+          <Link className="block text-center my-5 text-gray-500" to="/">
+            Iniciar sesión
+          </Link>
+        )}
+      </div>
     </>
   );
 };
--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -57,7 +57,7 @@ const Registrar = () => {
     // a traves de fetch api (nativo) o axios
     // con async/await o con promises
     try {
-      const url = "http://localhost:4000/api/veterinarios";
+      const url = `${import.meta.env.VITE_BACKEND_URL}/api/veterinarios`;
       await axios.post(url, { nombre, email, password });
       setAlerta({ msg: "Creado correctamente, revisa tu email", error: false });
     } catch (error) {
